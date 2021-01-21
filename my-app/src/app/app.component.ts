3751936f11741829eeb4eae634b813c55787a564
Igor14545216 <igor_ea@yahoo.com.br>
2021-01-21 19:37:11 -0300

    Add HttpClient para inicio das requisicoes no backend

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,14 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  photos = [
-    {
-      url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/5a/Sultan_the_Barbary_Lion.jpg/440px-Sultan_the_Barbary_Lion.jpg',
-      description: 'Leão'
-    },
-    {
-      url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2d/Lioness_Etosha_NP.jpg/500px-Lioness_Etosha_NP.jpg',
-      description: 'Leoa'
-    }
-  ];
+
+  constructor(http: HttpClient) {
+    console.log(http);
+   }
+
+  photos = [];
 }
