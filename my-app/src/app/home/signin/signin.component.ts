0e843e6d9280ad7bf64d3993626afa17f6fdacb0
Igor14545216 <igor_ea@yahoo.com.br>
2021-01-22 16:41:21 -0300

    Add formulario com validacao simples

--- my-app/src/app/home/signin/signin.component.ts
+++ my-app/src/app/home/signin/signin.component.ts
@@ -1,10 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
     templateUrl: './signin.component.html',
 })
 export class SigninComponent implements OnInit {
-    constructor() { }
 
-    ngOnInit(): void { }
+    loginForm: FormGroup
+
+    constructor(public formBuilder: FormBuilder) {
+        this.loginForm = this.formBuilder.group({
+            userName: ['', Validators.required],
+            password: ['', Validators.required]
+        })
+    }
+
+    ngOnInit(): void {
+    }
 }
