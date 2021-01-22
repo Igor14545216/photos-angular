0b9dc0107f8cc8e01a2de789b4de8832918a7458
Igor14545216 <igor_ea@yahoo.com.br>
2021-01-21 21:32:19 -0300

    usando rotas para exibir determinado component

--- my-app/src/app/app-routing.module.ts
+++ my-app/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { PhotoFormComponent } from './photos/photo-form/photo-form.component';
+import { PhotoListComponent } from './photos/photo-list/photo-list.component';
+
+const routes: Routes = [
+  //estou dizendo que, ao acessar a rota user/flavio, eu vou abrir o component PhotoListComponent
+  { path: 'user/flavio', component: PhotoListComponent },
+  { path: 'p/add', component: PhotoFormComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
