1dd91d144bdc781568219884b59861a40155a906
Igor14545216 <igor_ea@yahoo.com.br>
2021-01-22 14:15:56 -0300

    Passando parametro na diretiva

--- my-app/src/app/shared/diretivas/darken-on-over/darken-on-hover.directive.ts
+++ my-app/src/app/shared/diretivas/darken-on-over/darken-on-hover.directive.ts
@@ -1,13 +1,12 @@
-import { Directive, ElementRef, HostListener, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, HostListener, Input, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appDarkenOnHover]'
 })
 export class DarkenOnHoverDirective {
 
-  //ElementRef: elemento do DOM a qual esta diretiva está relacionada. Para manipular o DOM.
-  //@HostListener: para poder passar o evento 
-
+  @Input() brightness: string = '70%';
+  
   constructor(
     private el: ElementRef,
     private render: Renderer2) { }
@@ -15,8 +14,7 @@ export class DarkenOnHoverDirective {
 
   @HostListener('mouseover')
   darkenOn() {
-    // this.el.nativeElement vou pegar o elemento nativo de onde está a minha diretiva
-    this.render.setStyle(this.el.nativeElement, 'filter', 'brightness(70%)');
+    this.render.setStyle(this.el.nativeElement, 'filter', `brightness(${this.brightness})`);
   }
 
   @HostListener('mouseleave')
