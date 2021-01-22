1e6ef1f2b6118cfdbdc4aebc94b184de3798a544
Igor14545216 <igor_ea@yahoo.com.br>
2021-01-22 20:24:09 -0300

    Obtendo o token ao fazer login

---
+++ my-app/src/app/auth/auth.service.ts
@@ -0,0 +1,23 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { tap } from 'rxjs/operators'
+
+const API_URL = 'http://localhost:3000';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private http: HttpClient) { }
+
+  authenticate(userName: string, password: string) {
+    return this.http.post<any>(API_URL + '/user/login', { userName, password }, { observe: 'response' })
+      .pipe(tap(res => {
+        const authToken = res.headers.get('x-access-token');
+        console.log(userName + ' ' + authToken);
+        
+      }))   //pipe() usado para aplicar operação antes do subscripe
+  }
+
+}
