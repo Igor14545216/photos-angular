04ff0065afcd0abe78dce23f47fa09486efccbce
Igor14545216 <igor_ea@yahoo.com.br>
2021-01-22 00:56:18 -0300

    Add load button

--- my-app/src/app/photos/photo-list/photo-list.component.ts
+++ my-app/src/app/photos/photo-list/photo-list.component.ts
@@ -4,6 +4,7 @@ import { debounceTime } from 'rxjs/operators'
 import { Subject } from 'rxjs';
 
 import { Photo } from '../photo/photo';
+import { PhotoService } from '../photo/photo.service';
 
 @Component({
   selector: 'app-photo-list',
@@ -15,11 +16,16 @@ export class PhotoListComponent implements OnInit, OnDestroy {
   photos: Photo[] = [];
   filter: string = '';
   debounce: Subject<any> = new Subject<any>();
+  hasMore: boolean = true;
+  currentPage: number = 1;
+  userName: string = '';
 
   constructor(
-    private activatedRoute: ActivatedRoute) { }
+    private activatedRoute: ActivatedRoute, 
+    private service: PhotoService) { }
 
   ngOnInit() {
+    this.userName = this.activatedRoute.snapshot.params.userName;
     this.photos = this.activatedRoute.snapshot.data['photos'];
     this.debounce
       .pipe(debounceTime(300))
@@ -31,5 +37,14 @@ export class PhotoListComponent implements OnInit, OnDestroy {
   ngOnDestroy(): void {
     this.debounce.unsubscribe();
   }
+
+  load() {
+    this.service
+        .listFromUserPaginated(this.userName, ++this.currentPage)
+        .subscribe(photos => {
+            this.photos = this.photos.concat(photos)
+            if(!photos.length) this.hasMore = false;
+        });
+}
 
 }
