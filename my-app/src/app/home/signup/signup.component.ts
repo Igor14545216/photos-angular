51a06347c3ee101447c1eea3d36c4c722c231da1
Igor14545216 <igor_ea@yahoo.com.br>
2021-01-24 23:03:53 -0300

    Enviando dados via post

--- my-app/src/app/home/signup/signup.component.ts
+++ my-app/src/app/home/signup/signup.component.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
 import { lowerCaseValidator } from 'src/app/shared/validators/lower-case.validator';
 import { UserNotTakenValidatorService } from './user-not-taken.validator.service';
+import { SignUpService } from './signup.service';
+import { NewUser } from './new-user';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-signup',
@@ -12,6 +16,8 @@ export class SignupComponent implements OnInit {
     formulario: FormGroup;
 
     constructor(
+        private router: Router,
+        private service: SignUpService,
         private fb: FormBuilder,
         private userNotTakenValidatorService: UserNotTakenValidatorService) {
 
@@ -49,4 +55,16 @@ export class SignupComponent implements OnInit {
     }
 
     ngOnInit(): void { }
+
+    signup(e: any) {
+        const newUser = this.formulario.getRawValue() as NewUser;
+        this.service.signup(newUser).subscribe(() => {
+            this.router.navigate(['']);
+        },
+            err => {
+                console.log(err);
+                
+            }
+        )
+    }
 }
